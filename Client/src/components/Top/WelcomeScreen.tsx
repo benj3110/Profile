8bf1e582966252f32e62743d5f4b833d66fa11ec
benitovark <benito.varghese@raytheon.co.uk>
2024-04-05 09:42:43 +0100

    animated background

--- Client/src/components/Top/WelcomeScreen.tsx
+++ Client/src/components/Top/WelcomeScreen.tsx
@@ -1,5 +1,5 @@
 import styles from "../../css/Top/WelcomeScreen.module.scss"
-//import anime from 'animejs';
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
 
 const WelcomeScreen: React.FC = () => {
 
@@ -9,6 +9,15 @@ const WelcomeScreen: React.FC = () => {
             <h1 className={styles.Name}>Benito Varghese</h1>
             <p className={styles.Job}>Full-Stack Software Engineer</p>
             <div className={styles.Background}></div>
+            <Player
+                autoplay
+                loop
+                src="/LaptopAnimation.json"
+                className={styles.Animation}
+            // style={{ height: '400px', width: '400px' }}
+            >
+                <Controls visible={false} />
+            </Player>
         </section >);
 }
 
